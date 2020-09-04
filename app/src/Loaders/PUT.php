9871a78d6bfa208e2dba6a9969b2c5d183f89e19
Ian Walls <ian@bywatersolutions.com>
2020-09-04 21:04:59 +0000

    Fix PUT loader
    A variety of fixes to the PUT loader, based on usage experience,
    to make it function better/as intended.

--- app/src/Loaders/PUT.php
+++ app/src/Loaders/PUT.php
@@ -30,7 +30,7 @@ class PUT extends Loader
      *
      * @var mixed
      */
-    protected $key = ['id'];
+    protected $key = [];
 
     /**
      * Configuration for the POST request
@@ -86,44 +86,45 @@ class PUT extends Loader
      */
     public function load(Row $row)
     {
+        if ($this->key) {
+           $put_id = "/" . $row[$this->key[0]];
+        } else {
+           $put_id = '';
+        }
         if ($this->columns) {
             $result = [];
-
             foreach ($this->columns as $key => $column) {
                 if (isset($row[$key]) ) {
-                   if($row[$key] == 'true') {
-                      $results[$column] = true;
+                   if ($row[$key] == 'true') {
+                        $result[$column] = true;
                    } elseif ($row[$key] == 'false') {
-		      $results[$column] = false;
+                        $result[$column] = false;
                    } else {
-		      $result = $row[$key];
+                        $result[$column] = $row[$key];
                    }
 		} else {
 		   $result[$column] = null;
 		}
             }
-            $interim = array();
+	    $interim = array();
             foreach($result as $path => $value) {
               $ancestors = explode('.', $path);
               $this->set_nested_value($interim, $ancestors, $value);
             }
-
             $row = $interim;
+        } elseif (isset($row['record'])) {
+                $row = $row['record'];
         }
 
-	if ($this->transaction) {
-		// requires a bulk API
-	} else {
-		$client = $this->rest->getConnection($this->connection);
-		$payload = $this->config;
-		$payload['json'] = $row;
-		$payload['headers']['Accept'] = 'text/plain';
-		try {
-			$response = $client->put($this->output . "/" . $row[$this->key[0]], $payload);
-		} catch (RequestException $e) {
-			if ($e->hasResponse()) {
-			        echo Psr7\str($e->getResponse());
-    			}
+	$client = $this->rest->getConnection($this->connection);
+	$payload = $this->config;
+	$payload['json'] = $row;
+	$payload['headers']['Accept'] = 'text/plain';
+	try {
+		$response = $client->put($this->output . $put_id, $payload);
+	} catch (RequestException $e) {
+		if ($e->hasResponse()) {
+		        echo Psr7\str($e->getResponse());
 		}
 	}
     }
@@ -137,42 +138,26 @@ class PUT extends Loader
     {
     }
 
-    // borrowed shamelessly from Stack Overflow (https://stackoverflow.com/questions/9628176/using-a-string-path-to-set-nested-array-data)
-    private function set_nested_array_value(&$array, $path, &$value, $delimiter = '/') {
-      $pathParts = explode($delimiter, $path);
-
-      $current = &$array;
-      foreach($pathParts as $key) {
-          $current = &$current[$key];
-      }
-
-      $backup = $current;
-      $current = $value;
-
-      return $backup;
-   }
-
-   /**
-    * Give it and array, and an array of parents, it will decent into the
-    * nested arrays and set the value.
-    */
+    /**
+     * Give it and array, and an array of parents, it will decent into the
+     * nested arrays and set the value.
+     */
     function set_nested_value(array &$arr, array $ancestors, $value) {
-      $current = &$arr;
-      foreach ($ancestors as $key) {
+	$current = &$arr;
+  	foreach ($ancestors as $key) {
+	    // To handle the original input, if an item is not an array, 
+    	    // replace it with an array with the value as the first item.
+    	    if (!is_array($current)) {
+      	 	$current = array( $current);
+    	    }
 
-        // To handle the original input, if an item is not an array, 
-        // replace it with an array with the value as the first item.
-        if (!is_array($current)) {
-          $current = array( $current);
+	    if (!array_key_exists($key, $current)) {
+  	        $current[$key] = array();
+    	    }
+	    $current = &$current[$key];
         }
 
-        if (!array_key_exists($key, $current)) {
-          $current[$key] = array();
-        }
-        $current = &$current[$key];
-      }
-
-      $current = $value;
+	$current = $value;
     }
 }
 
