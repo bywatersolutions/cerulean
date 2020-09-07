c85965f73615b6f63f0353697169f4740ec16ef2
Ian Walls <ian@bywatersolutions.com>
2020-09-07 14:37:44 +0000

    Add support for multiple placeholders in Default Transformer
    Let's multiple other fields be referenced by {{fieldname}} in
    the Defaults transformer, i.e. {{field1}}.{{field2}}

--- app/src/Transformers/Defaults.php
+++ app/src/Transformers/Defaults.php
@@ -47,9 +47,9 @@ class Defaults extends Transformer
      */
     public function transform(Row $row)
     {
-	foreach ($this->columns as $key => $value) {
-	    if ( !isset($row[$key]) || is_null($row[$key]) || $row[$key] == '' || $this->force ) {
-		// if the value is wrapped in {}, lookup that row if extant
+        foreach ($this->columns as $key => $value) {
+            if ( !isset($row[$key]) || is_null($row[$key]) || $row[$key] == '' || $this->force ) {
+                // if the value is wrapped in {}, lookup that row if extant
                 if (preg_match_all('/\{(?P<column>[^\}]+)\}/', $value, $matches) ) {
                    foreach($matches['column'] as $column) {
                         if (isset($row[$column]) ) {
